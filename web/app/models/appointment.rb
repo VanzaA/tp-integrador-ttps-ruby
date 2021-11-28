5b97409872983ae749a6fb96b2304ba083eb42cf
vanza <agustin.vanza@gmail.com>
2021-11-28 20:09:42 -0300

    Updated models

--- web/app/models/appointment.rb
+++ web/app/models/appointment.rb
@@ -5,5 +5,11 @@ class Appointment < ApplicationRecord
   validates :surname, presence: { message: "El apellido del paciente es obligatorio" }
   validates :phone, presence: { message: "El telefono es obligatorio" }
   validates :date, uniqueness: { scope: :professional_id, message: "Ya existe un turno para este profesional" }
+  validate :date_cannot_be_in_the_past
 
+  def date_cannot_be_in_the_past
+    if date.present? && date < DateTime.now
+      errors.add(:date, "El turno no puede ser asignado a una fecha y hora menor a la actual")
+    end
+  end
 end
--- web/app/models/professional.rb
+++ web/app/models/professional.rb
@@ -1,7 +1,8 @@
 class Professional < ApplicationRecord
+  has_many :appointments, dependent: :destroy
   validates :name, uniqueness: { scope: :surname, message: "Ya existe un profesional con ese nombre y apellido" }
 
   def full_name
-    "#{surname}, #{name}"
+    "#{surname.capitalize}, #{name.capitalize}"
   end
 end
