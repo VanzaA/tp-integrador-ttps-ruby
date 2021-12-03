c2011b3e156e2e17641d117165da97b273d35160
vanza <agustin.vanza@gmail.com>
2021-12-03 00:10:13 -0300

    Added not authorized view

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to session_path
   end
+
+  rescue_from ActionPolicy::Unauthorized do |ex|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
 end
